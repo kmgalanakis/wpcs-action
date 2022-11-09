722158b8b65754b9c2c2955bb5bb33247659e4ac
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-11-09 19:33:17 +0300

    10up-Default added

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ if [ "${INPUT_STANDARD}" = "WordPress-VIP-Go" ] || [ "${INPUT_STANDARD}" = "Word
     git clone https://github.com/Automattic/VIP-Coding-Standards ${HOME}/vipcs
     git clone https://github.com/sirbrillig/phpcs-variable-analysis ${HOME}/variable-analysis
     ${INPUT_PHPCS_BIN_PATH} --config-set installed_paths "${HOME}/wpcs,${HOME}/vipcs,${HOME}/variable-analysis"
+elif [ "${INPUT_STANDARD}" = "10up-Default" ]; then
+    echo "Setting up 10up-Default"
+    git clone https://github.com/10up/phpcs-composer ${HOME}/10up
+    ${INPUT_PHPCS_BIN_PATH} --config-set installed_paths "${HOME}/wpcs,${HOME}/10up/10up-Default"
 elif [ -z "${INPUT_STANDARD_REPO}" ] || [ "${INPUT_STANDARD_REPO}" = "false" ]; then
     ${INPUT_PHPCS_BIN_PATH} --config-set installed_paths ~/wpcs
 else
